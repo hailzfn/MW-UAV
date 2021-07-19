a67e69b51b01c1d75b7ca69f016f2a1e7ee0a9d2
Devkush (Cyril) <78838322+Devkush@users.noreply.github.com>
2021-07-19 02:56:43 +0200

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,92 @@
+﻿
+namespace UAV_tool
+{
+    partial class Form1
+    {
+        private System.ComponentModel.IContainer components = null;
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.UAV_ON = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.SuspendLayout();
+            // 
+            // UAV_ON
+            // 
+            this.UAV_ON.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.UAV_ON.Location = new System.Drawing.Point(8, 8);
+            this.UAV_ON.Margin = new System.Windows.Forms.Padding(2);
+            this.UAV_ON.Name = "UAV_ON";
+            this.UAV_ON.Size = new System.Drawing.Size(263, 27);
+            this.UAV_ON.TabIndex = 0;
+            this.UAV_ON.Text = "TRAINER (ON)";
+            this.UAV_ON.UseVisualStyleBackColor = true;
+            this.UAV_ON.Click += new System.EventHandler(this.UAV_ON_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 16;
+            this.listBox1.Location = new System.Drawing.Point(8, 100);
+            this.listBox1.Margin = new System.Windows.Forms.Padding(2);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(264, 52);
+            this.listBox1.TabIndex = 1;
+            this.listBox1.TabStop = false;
+            // 
+            // backgroundWorker1
+            // 
+            this.backgroundWorker1.DoWork += new System.ComponentModel.DoWorkEventHandler(this.backgroundWorker1_DoWork);
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.checkBox1.Location = new System.Drawing.Point(12, 52);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(71, 28);
+            this.checkBox1.TabIndex = 3;
+            this.checkBox1.Text = "UAV";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(278, 168);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.UAV_ON);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(2);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.Text = "UAV Tool";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        private System.Windows.Forms.Button UAV_ON;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private System.Windows.Forms.CheckBox checkBox1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,158 @@
+﻿using System;
+using System.Diagnostics;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+using System.Drawing;
+
+namespace UAV_tool
+{
+    public partial class Form1 : Form
+    {
+
+        [DllImport("kernel32.dll")]
+        public static extern bool WriteProcessMemory(IntPtr hProcess, Int64 lpBaseAddress, byte[] lpBuffer, int dwSize, ref int lpNumberOfBytesWritten);
+
+        [DllImport("kernel32.dll")]
+        public static extern bool ReadProcessMemory(IntPtr hProcess, Int64 lpBaseAddress, byte[] lpBuffer, int dwSize, ref int lpNumberOfBytesRead);        
+        
+        [DllImport("user32.dll", SetLastError = true)]
+        static extern IntPtr FindWindowEx(IntPtr hwndParent, IntPtr hwndChildAfter, string lpszClass, string lpszWindow);
+
+
+   
+        public static long UAV_Base = ModuleBase(0x16CA0FB8); 
+        public long Offset2 = UAV_Base + 0x20;       
+        public long Offset3 = UAV_Base + 0x18;     
+        public long Offset4 = UAV_Base + 0x1C;    
+        public long Offset5 = UAV_Base + 0x38;       
+        public long Offset6 = UAV_Base + 0x3C;
+
+
+        public bool trainerOn = false;
+
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            Process p = Process.GetProcessesByName("ModernWarfare")[0];
+            
+            string message = "Warzone Found!";
+            MessageBox.Show(message);
+        }
+
+
+        public static Int64 ModuleBase(long BaseOffset)
+        {
+            Process p = Process.GetProcessesByName("ModernWarfare")[0];
+            IntPtr processHandle = p.Handle;
+            Byte[] buffer = new Byte[8];
+            Int32 bytesRead = 0;
+
+            long BaseAddress = p.MainModule.BaseAddress.ToInt64() + BaseOffset;
+            ReadProcessMemory(processHandle, BaseAddress, buffer, buffer.Length, ref bytesRead);
+            Int64 baseValue = BitConverter.ToInt64(buffer, 0);
+
+            return ((baseValue + 0x10) - p.MainModule.BaseAddress.ToInt64());
+        }
+
+        private void UAV_ON_Click(object sender, EventArgs e)
+        {
+            
+
+            trainerOn = !trainerOn;
+
+            if (trainerOn)
+            {
+                UAV_ON.ForeColor = Color.Green;
+                UAV_ON.Text = ("TRAINER (OFF)");
+            }
+            else
+            {
+                UAV_ON.ForeColor = Color.Red;
+                UAV_ON.Text = ("TRAINER (ON)");
+            }
+
+            if (checkBox1.Checked)
+            {
+                listBox1.Items.Clear();
+                listBox1.Items.Add("");
+                listBox1.Items.Add("UAV ON !");
+            }
+            if (!checkBox1.Checked)
+            {
+                listBox1.Items.Clear();
+                listBox1.Items.Add("");
+                listBox1.Items.Add("UAV OFF !");
+            }
+        }
+
+        private void backgroundWorker1_DoWork(object sender, System.ComponentModel.DoWorkEventArgs e)
+        {
+
+            while (true)
+            {
+                try
+                {
+                    if (checkBox1.Checked)
+                    {
+                        int BytesWritten = 0;
+                        Process p = Process.GetProcessesByName("ModernWarfare")[0];
+
+                        IntPtr processHandle = p.Handle;
+                        Int64 BaseAddress = p.MainModule.BaseAddress.ToInt64();
+
+                        var ON_1 = new byte[] { 0x01, 0x00, 0x00, 0x00 };
+                        var ON_2 = new byte[] { 0x01, 0x00, 0x00, 0x00 };
+                        var ON_3 = new byte[] { 0x4f, 0xbb, 0x55, 0x51 };
+                        var ON_4 = new byte[] { 0xf6, 0x6d, 0xae, 0x79 };
+                        var ON_5 = new byte[] { 0x4f, 0xbb, 0x55, 0x51 };
+                        var ON_6 = new byte[] { 0xf6, 0x6d, 0xae, 0x79 };
+
+
+                        WriteProcessMemory(processHandle, BaseAddress + UAV_Base, ON_1, ON_1.Length, ref BytesWritten);
+                        WriteProcessMemory(processHandle, BaseAddress + Offset2, ON_2, ON_2.Length, ref BytesWritten);
+                        WriteProcessMemory(processHandle, BaseAddress + Offset3, ON_3, ON_3.Length, ref BytesWritten);
+                        WriteProcessMemory(processHandle, BaseAddress + Offset4, ON_4, ON_4.Length, ref BytesWritten);
+                        WriteProcessMemory(processHandle, BaseAddress + Offset5, ON_5, ON_5.Length, ref BytesWritten);
+                        WriteProcessMemory(processHandle, BaseAddress + Offset6, ON_6, ON_6.Length, ref BytesWritten);
+
+                        
+                    }
+                    if (!checkBox1.Checked)
+                    {
+                        int BytesWritten = 0;
+                        Process p = Process.GetProcessesByName("ModernWarfare")[0];
+
+                        IntPtr processHandle = p.Handle;
+                        Int64 BaseAddress = p.MainModule.BaseAddress.ToInt64();
+
+                        var OFF_1 = new byte[] { 0x00, 0x00, 0x00, 0x00 };
+                        var OFF_2 = new byte[] { 0x00, 0x00, 0x00, 0x00 };
+                        var OFF_3 = new byte[] { 0x00, 0x00, 0x00, 0x00 };
+                        var OFF_4 = new byte[] { 0x00, 0x00, 0x00, 0x00 };
+                        var OFF_5 = new byte[] { 0x00, 0x00, 0x00, 0x00 };
+                        var OFF_6 = new byte[] { 0x00, 0x00, 0x00, 0x00 };
+
+
+                        WriteProcessMemory(processHandle, BaseAddress + UAV_Base, OFF_1, OFF_1.Length, ref BytesWritten);
+                        WriteProcessMemory(processHandle, BaseAddress + Offset2, OFF_2, OFF_2.Length, ref BytesWritten);
+                        WriteProcessMemory(processHandle, BaseAddress + Offset3, OFF_3, OFF_3.Length, ref BytesWritten);
+                        WriteProcessMemory(processHandle, BaseAddress + Offset4, OFF_4, OFF_4.Length, ref BytesWritten);
+                        WriteProcessMemory(processHandle, BaseAddress + Offset5, OFF_5, OFF_5.Length, ref BytesWritten);
+                        WriteProcessMemory(processHandle, BaseAddress + Offset6, OFF_6, OFF_6.Length, ref BytesWritten);
+                    }
+                }
+                catch (Exception err)
+                {
+                    listBox1.Items.Add(err);
+                }
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            if (!backgroundWorker1.IsBusy) backgroundWorker1.RunWorkerAsync();
+        }
+    }
+}
